aeb1fe5228776a4d1dd613bee1279a6830f2c265
jeffmiko <jeff.miko@gmail.com>
2021-11-11 12:09:56 -0500

    Renamed some files/classes

--- tests/api.js
+++ tests/api.js
@@ -1,11 +1,11 @@
-const {RestApp, RestMySql, RestBeforeHooks, RestAfterHooks} = require("../lib");
+const {RestApp, RestMySqlRequest, RestBeforeHooks, RestAfterHooks} = require("../lib");
 
 
 const rest = new RestApp()
 const tables = ["users"]
 
 for(let tbl of tables) {
-  rest.use(new RestMySql(tbl))  
+  rest.use(new RestMySqlRequest(tbl))  
   rest.service(tbl).hooks()
     .before(RestBeforeHooks.isoToDate(["created", "updated"]))
     .after(RestAfterHooks.stripNulls())
